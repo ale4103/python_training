fffbf516595e597a1acc45397eb63ec27ac0172e
alexander.ivanov <ale4103@gmail.com>
2019-03-18 16:38:12 +0300

    add parameters to methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -14,31 +14,32 @@ class TestAddContact(unittest.TestCase):
         self.open_main_page(wd)
         self.login(wd)
         self.create_contact(wd)
+        self.logout(wd)
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, firstname="sdfdsf", lastname="sdfsdfsdfsd", address="fdfsfdsfsfsd", mobile="9797987987"):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("sdfdsf")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("sdfsdfsdfsd")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("fdfsfdsfsfsd")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("9797987987")
+        wd.find_element_by_name("mobile").send_keys(mobile)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def login(self, wd):
+    def login(self, wd, username="admin", password="secret"):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def logout(self, wd):
