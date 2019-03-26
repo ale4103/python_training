a7555a0f6233237a400b2ee6d2b1e7500a6de4f1
Alexander Ivanov4 <alexander_ivanov4@epam.com>
2019-03-27 01:50:30 +0300

    Classes for contact and group were created

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,5 +12,5 @@ def app(request):
 
 def test_create_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="sdfdsf", lastname="sdfsdfsdfsd", address="fdfsfdsfsfsd", mobile="9797987987"))
+    app.contact.create(Contact(firstname="sdfdsf", lastname="sdfsdfsdfsd", address="fdfsfdsfsfsd", mobile="9797987987"))
     app.session.logout()
