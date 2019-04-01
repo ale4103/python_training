84b89bfa22aef8d6244a633cc8b576139f822a51
alexander.ivanov <ale4103@gmail.com>
2019-04-01 14:57:23 +0300

    Preconditions for groups and contacts were added

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,4 +2,6 @@
 from model.contact import Contact
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="sdfdsf"))
     app.contact.edit_first_contact(Contact(firstname="Aewjkerh", lastname="fksdjl", address="jgri", mobile="9797985555"))
