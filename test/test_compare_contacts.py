22ba07e4551c4bbbe0e89ac72cdda85ec6f88a9c
Alexander Ivanov4 <alexander_ivanov4@epam.com>
2019-04-14 17:28:46 +0300

    check for random contact

--- test/test_compare_contacts.py
+++ test/test_compare_contacts.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
+from random import randrange
 
 def test_compare_contacts_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact = app.contact.get_contact_list()
+    index = randrange(len(contact))
+    contact_from_home_page = app.contact.get_contact_list()[index-1]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index-1)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
