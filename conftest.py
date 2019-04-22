a98ca0eac60b02bca7ee8366277a70b01687bfda
alexander.ivanov <ale4103@gmail.com>
2019-04-22 15:34:41 +0300

    browser option was changed

--- conftest.py
+++ conftest.py
@@ -33,7 +33,7 @@ def stop(request):
     return fixture
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="target.json")
+    parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
 
 def pytest_generate_tests(metafunc):
