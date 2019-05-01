75a1a8f195abce8b3f388bbb07f4261c182cd397
Alexander Ivanov4 <alexander_ivanov4@epam.com>
2019-05-01 12:10:31 +0300

    check all contacts from home page and from DB

--- fixture/db.py
+++ fixture/db.py
@@ -37,3 +37,16 @@ class DbFixture:
 
     def destroy(self):
         self.connection.close()
+
+    def get_contact_list_as_at_ui(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(
+                "select id, firstname, lastname, address, email, email2, email3, home, mobile, work, phone2 from addressbook where deprecated = '0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, email, email2, email3, home, mobile, work, fax) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, mobile=mobile))
+        finally:
+            cursor.close()
+        return list
--- test/test_compare_contacts.py
+++ test/test_compare_contacts.py
@@ -14,8 +14,18 @@ def test_compare_contacts_on_home_page(app):
     assert contact_from_home_page.all_emails == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
-
-
+def test_compare_contacts_on_home_page_and_db(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="sdfdsf"))
+    contacts_from_home_page = app.contact.get_contact_list()
+    contact_from_data_base = db.get_contact_list_as_at_ui()
+    assert len(contacts_from_home_page) == len(contact_from_data_base)
+    contacts_from_home_page_sorted = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    contact_from_data_base_sorted = sorted(contact_from_data_base, key=Contact.id_or_max)
+    for x in range(len(contacts_from_home_page_sorted)):
+        assert contacts_from_home_page_sorted[x].firstname == contact_from_data_base_sorted[x].firstname.strip()
+        assert contacts_from_home_page_sorted[x].lastname == contact_from_data_base_sorted[x].lastname.strip()
+        assert contacts_from_home_page_sorted[x].address == contact_from_data_base_sorted[x].address
 
 def clear(s):
     return re.sub("[() -]", "", s)
